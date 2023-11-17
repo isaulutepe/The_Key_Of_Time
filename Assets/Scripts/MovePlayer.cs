38dfc2fdc7764fa67bdd67223da5952e0b04a259
İsa <ulutepe42@gmail.com>
2023-11-17 23:36:43 +0300

    Fixed problem

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -13,11 +13,14 @@ public class MovePlayer : MonoBehaviour
     private Vector3 movement;
     private bool jump = false;
     private bool isGround = true; //Karekter yerde.
+    private bool isMove = false;
+    [SerializeField] private Animator animator;
 
     private void Awake()
     {
         rg = GetComponent<Rigidbody>();
         rg.drag = 1; //Yerçekimini simile etmek için gerçekçi düþüþ vermek için.
+
     }
     private void FixedUpdate()
     {
@@ -25,14 +28,25 @@ public class MovePlayer : MonoBehaviour
         {
             Debug.Log("Sýplanýyor.");
             rg.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-            jump= false;
+            jump = false;
         }
     }
     private void Update()
     {
+        //Animasyonlarý kontrol etmek için.
+        if (isMove == false)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        if (isMove == true)
+        {
+            animator.SetBool("isMoving", true);
+        }
+
         movePlayer();
         if (Input.GetKey(KeyCode.Space) && isGround == true)
         {
+
             jump = true;
             isGround = false; //Karekter havada
         }
@@ -49,8 +63,18 @@ public class MovePlayer : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
         movement = new Vector3(horizontalInput, 0f, verticalInput);
         rg.MovePosition(rg.position + movement * speed * Time.deltaTime);
+
+        if (movement != Vector3.zero) // hareket varsa
+        {
+            transform.rotation = Quaternion.LookRotation(movement); // karakteri hareket yönüne döndür
+            isMove = true;
+        }
+        if (movement == Vector3.zero)
+        {
+            isMove = false;
+        }
+
     }
 }
