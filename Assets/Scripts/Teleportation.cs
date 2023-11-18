784fb2940246e02248b68a7eaa95ca3c54eebb63
İsa <ulutepe42@gmail.com>
2023-11-18 14:09:58 +0300

    Added teleport Shader and shader material and Fixed Teleportation Script

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -8,7 +8,9 @@ public class Teleportation : MonoBehaviour
 
     GameObject player;
     [SerializeField] private Vector3 destinationPos;
-    
+    bool finishedShader = false;
+    float dissolveAmount = 0;
+
 
     private void Awake()
     {
@@ -24,19 +26,35 @@ public class Teleportation : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            player.GetComponent<MovePlayer>().enabled= false;
+            Debug.Log("ýþýnlanma baþladý");
+            player.GetComponent<MovePlayer>().enabled = false;
             teleportation();
         }
     }
     void teleportation()
     {
+        StartCoroutine(DissolveMaterial()); //Shader çaðýr
+        StartCoroutine(goTeleport()); //Bekle ve isinla
+        StartCoroutine(waitTeleport()); //Telepor kapat.
+    }
+    IEnumerator goTeleport()
+    {
+        yield return new WaitForSeconds(0.5f);
         player.transform.position = destinationPos;
-        StartCoroutine(waitTeleport());
+    }
+    IEnumerator DissolveMaterial()
+    {
+        while (dissolveAmount < 1)
+        {
+            dissolveAmount += Time.deltaTime * 0.5f; // Bu deðeri ayarlayarak çözülme hýzýný kontrol edebilirsiniz.
+            player.transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material.SetFloat("_DissolveAmount", dissolveAmount);
+            yield return null;
+        }
     }
     IEnumerator waitTeleport()
     {
-        yield return new WaitForSeconds(0.1f);
-        player.GetComponent<MovePlayer>().enabled= true;
+        yield return new WaitForSeconds(0.5f);
+        player.GetComponent<MovePlayer>().enabled = true;
 
     }
 
